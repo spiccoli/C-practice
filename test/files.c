bf6c876d2d6449c3c5282554f2d72b4cd7da3947
admin <piccolisantiagonicolas@gmail.com>
2024-06-27 16:48:37 -0300

    linkear paginas en c

---
+++ test/files.c
@@ -0,0 +1,8 @@
+/*tratamos de importar de un archivo una funcion*/
+#include <stdio.h>
+int sumar(int x, int y);
+
+int main(int argc, char const *argv[]) {
+printf("suma es %d\n", sumar(10,20));
+return 0;
+}
---
+++ test/funcion.c
@@ -0,0 +1,4 @@
+/*Sumar dos enteros*/
+int sumar(int x, int y){
+    return x + y;
+};

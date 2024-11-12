415f75bf5f2de1b4875d5f086a54000a5ce8efb9
Santiago <piccolisantiagonicolas@gmail.com>
2024-11-12 20:39:50 -0300

    ex51

--- prog 1/64excercises/51.c
+++ prog 1/64excercises/51.c
@@ -10,8 +10,81 @@ Se pide realizar 3 listados por pantalla:
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(){
+#define MAX_EMP 100 // Número máximo de empleados
 
-return 0;
+typedef struct {
+    char nombre[50];
+    char calle[50];
+    int numero;
+    char localidad[50];
+    int edad;
+    char sexo;
+} Empleado;
+
+void ingresarEmpleado(Empleado *emp) {
+    printf("Nombre: ");
+    scanf("%s", emp->nombre);
+    printf("Calle: ");
+    scanf("%s", emp->calle);
+    printf("Número: ");
+    scanf("%d", &emp->numero);
+    printf("Localidad: ");
+    scanf("%s", emp->localidad);
+    printf("Edad: ");
+    scanf("%d", &emp->edad);
+    printf("Sexo (F/M): ");
+    scanf(" %c", &emp->sexo);
+}
+
+void listarEmpleadosMenoresDe25(Empleado empleados[], int count) {
+    printf("\nEmpleados menores de 25 años:\n");
+    for (int i = 0; i < count; i++) {
+        if (empleados[i].edad < 25) {
+            printf("%s, %s %d, %s, %d, %c\n", empleados[i].nombre, empleados[i].calle, empleados[i].numero, empleados[i].localidad, empleados[i].edad, empleados[i].sexo);
+        }
+    }
+}
+
+void listarEmpleadosEnPilar(Empleado empleados[], int count) {
+    printf("\nEmpleados que viven en Pilar:\n");
+    for (int i = 0; i < count; i++) {
+        if (strcmp(empleados[i].localidad, "Pilar") == 0) {
+            printf("%s, %s %d, %s, %d, %c\n", empleados[i].nombre, empleados[i].calle, empleados[i].numero, empleados[i].localidad, empleados[i].edad, empleados[i].sexo);
+        }
+    }
+}
+
+void listarEmpleadosEnPilarMenoresDe25(Empleado empleados[], int count) {
+    printf("\nEmpleados que viven en Pilar y son menores de 25 años:\n");
+    for (int i = 0; i < count; i++) {
+        if (empleados[i].edad < 25 && strcmp(empleados[i].localidad, "Pilar") == 0) {
+            printf("%s, %s %d, %s, %d, %c\n", empleados[i].nombre, empleados[i].calle, empleados[i].numero, empleados[i].localidad, empleados[i].edad, empleados[i].sexo);
+        }
+    }
+}
+
+int main() {
+    Empleado empleados[MAX_EMP];
+    int count = 0;
+    char continuar;
+
+    do {
+        if (count >= MAX_EMP) {
+            printf("Se alcanzó el número máximo de empleados.\n");
+            break;
+        }
+        ingresarEmpleado(&empleados[count]);
+        count++;
+
+        printf("¿Desea continuar ingresando empleados? (S/N): ");
+        scanf(" %c", &continuar);
+    } while (continuar == 'S' || continuar == 's');
+
+    listarEmpleadosMenoresDe25(empleados, count);
+    listarEmpleadosEnPilar(empleados, count);
+    listarEmpleadosEnPilarMenoresDe25(empleados, count);
+
+    return 0;
 }
